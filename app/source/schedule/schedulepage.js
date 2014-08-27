1ea849a0d72149b5988cf170f1a1a409680fde2e
Roman Zaynetdinov <romanznet@gmail.com>
2014-08-27 18:01:20 +0300

    Remove unused libs and Make pages indexing with analytics

--- app/source/schedule/schedulepage.js
+++ app/source/schedule/schedulepage.js
@@ -13,6 +13,32 @@ require('./util');
 
 Backbone.$ = $;
 
+// https://github.com/kendagriff/backbone.analytics
+var loadUrl = Backbone.History.prototype.loadUrl;
+
+Backbone.History.prototype.loadUrl = function () {
+  var
+    matched    = loadUrl.apply(this, arguments),
+    gaFragment = this.fragment;
+
+  if(!/^\//.test(gaFragment)) {
+    gaFragment = '/' + gaFragment;
+  }
+
+  var ga;
+  if(window.GoogleAnalyticsObject && window.GoogleAnalyticsObject !== 'ga') {
+    ga = window.GoogleAnalyticsObject;
+  } else {
+    ga = window.ga;
+  }
+
+  if (typeof ga !== 'undefined') {
+    ga('send', 'pageview', gaFragment);
+  }
+
+  return matched;
+};
+
 $(document).ready(function () {
 
   window.app = {};
