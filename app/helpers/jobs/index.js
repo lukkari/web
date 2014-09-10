1fdc93c43877eca0f27cfed3fca3a04c123bff8e
Roman Zaynetdinov <romanznet@gmail.com>
2014-09-10 11:53:59 +0300

    Add runParser job

--- app/helpers/jobs/index.js
+++ app/helpers/jobs/index.js
@@ -2,5 +2,14 @@
  * Main jobs configuration file (runs all jobs)
  */
 
+var CronJob   = require('cron').CronJob;
+
 // Run parser job
-// require('./runParser');
+var runParser = require('./runParser');
+
+new CronJob(
+  '00 05 9 * * *',
+  runParser,
+  null,
+  true
+);
