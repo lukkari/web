d6b8b834b1b727bf091d68cdf483d8e876c867be
Roman Zaynetdinov <romanznet@gmail.com>
2014-11-19 17:06:25 +0200

    Update week and weekday libs

--- app/libs/week.js
+++ app/libs/week.js
@@ -3,45 +3,40 @@
  */
 var weekDay  = require('./weekday');
 
-var week = {
+exports.getSchedule = function (options) {
+  options = options || {};
 
-  getSchedule : function (options) {
-    options = options || {};
+  var date = new Date(options.date);
 
-    var date = new Date(options.date);
+  // If saturday or sunday get next week
+  if((date.getDay() === 0) || date.getDay() == 6) date.setDate(date.getDate() + 2);
 
-    // If saturday or sunday get next week
-    if((date.getDay() === 0) || date.getDay() == 6) date.setDate(date.getDate() + 2);
+  date.setDate(date.getDate() - date.getDay() + 1);
 
-    date.setDate(date.getDate() - date.getDay() + 1);
+  var data  = [];
+  var count = 0;
+  var cb = function (err, subjects) {
+    data.push(subjects);
+    count += 1;
 
-    var data  = [];
-    var count = 0;
-    var cb = function (err, subjects) {
-      data.push(subjects);
-      count += 1;
-
-      if(err || (count > 4)) {
-        // Sort by day
-        data.sort(function (a, b) {
-          return Date.parse(a.date) - Date.parse(b.date);
-        });
-
-        options.cb(err, data);
-      }
-    };
-
-    for(var i = 0; i < 5; i += 1) {
-      weekDay.getSubjects({
-        date   : new Date(date.getFullYear(), date.getMonth(), date.getDate() + i),
-        type   : options.type,
-        typeid : options.typeid,
-        usertable : options.usertable,
-        cb : cb
+    if(err || (count > 4)) {
+      // Sort by day
+      data.sort(function (a, b) {
+        return Date.parse(a.date) - Date.parse(b.date);
       });
+
+      options.cb(err, data);
     }
+  };
+
+  for(var i = 0; i < 5; i += 1) {
+    weekDay.getSubjects({
+      date   : new Date(date.getFullYear(), date.getMonth(), date.getDate() + i),
+      type   : options.type,
+      typeid : options.typeid,
+      usertable : options.usertable,
+      cb : cb
+    });
   }
 
 };
-
-module.exports = week;
--- app/libs/weekday.js
+++ app/libs/weekday.js
@@ -4,84 +4,78 @@
 var mongoose = require('mongoose');
 var Entry = mongoose.model('Entry');
 
-var weekDay = {
+exports.getSubjects = function (options) {
+  options = options || {};
 
-  getSubjects : function (options) {
-    options = options || {};
+  var date = new Date(options.date);
 
-    var date = new Date(options.date);
+  var data = {
+    date : date,
+    subjects : []
+  };
 
-    var data = {
-      date : date,
-      subjects : []
+  var start = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  var end   = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+
+  /**
+   * QUERY
+   *
+   *  $and : [{
+   *    date : {
+   *      $gte : start,
+   *      $lt  : end
+   *    }
+   *  }, {
+   *    subject : { $nin : usertable.removed }
+   *  }, {
+   *    $or : [{
+   *      type : typeid
+   *    }, {
+   *      subject : { $in : usertable.added }
+   *    }]
+   *  }]
+   *
+   */
+
+  var usertable = options.usertable || null;
+
+  var query = {};
+  var qtype = {};
+
+  query[options.type] = options.typeid;
+
+  if(!usertable) {
+    query.date = {
+      $gte : start,
+      $lt  : end
     };
-
-    var start = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    var end   = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
-
-    /**
-     * QUERY
-     *
-     *  $and : [{
-     *    date : {
-     *      $gte : start,
-     *      $lt  : end
-     *    }
-     *  }, {
-     *    subject : { $nin : usertable.removed }
-     *  }, {
-     *    $or : [{
-     *      type : typeid
-     *    }, {
-     *      subject : { $in : usertable.added }
-     *    }]
-     *  }]
-     *
-     */
-
-    var usertable = options.usertable || null;
-
-    var query = {};
-    var qtype = {};
-
-    query[options.type] = options.typeid;
-
-    if(!usertable) {
-      query.date = {
+  } else {
+    query.$and = [{
+      'date' : {
         $gte : start,
         $lt  : end
-      };
-    } else {
-      query.$and = [{
-        'date' : {
-          $gte : start,
-          $lt  : end
-        }
-      }, {
-        subject : { $nin : usertable.removed }
-      }];
+      }
+    }, {
+      subject : { $nin : usertable.removed }
+    }];
 
-      query.$and.push({
-        $or : [qtype, {
-          subject : { $in : usertable.added }
-        }]
-      });
-    }
-
-    Entry
-      .find(query, { 'parse' : 0 })
-      .populate('groups', 'name')
-      .populate('teachers', 'name')
-      .populate('rooms', 'name')
-      .populate('subject', 'name')
-      .sort({ 'date' : 1 })
-      .exec(function(err, subjects) {
-        data.subjects = subjects;
-        if(typeof options.cb === 'function') options.cb(err, data);
-      });
-
+    query.$and.push({
+      $or : [qtype, {
+        subject : { $in : usertable.added }
+      }]
+    });
   }
 
-};
+  Entry
+    .find(query, { 'parse' : 0 })
+    .populate('groups', 'name')
+    .populate('teachers', 'name')
+    .populate('rooms', 'name')
+    .populate('subject', 'name')
+    .sort({ 'date' : 1 })
+    .exec(function(err, subjects) {
+      data.subjects = subjects;
+      if(typeof options.cb === 'function') options.cb(err, data);
+    });
 
-module.exports = weekDay;
+};
