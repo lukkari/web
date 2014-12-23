a9ff631ba25dd644b52caf63f339d096ef6ccd8f
Roman Zaynetdinov <romanznet@gmail.com>
2014-12-23 17:46:40 +0200

    Prepare for entry processing

--- app/routes/socket/manage.js
+++ app/routes/socket/manage.js
@@ -4,6 +4,9 @@
 
 var mongoose = require('mongoose');
 
+var Queue = require('../../libs/queue');
+var queue = new Queue();
+
 // DB Models
 var Group = mongoose.model('Group');
 var Teacher = mongoose.model('Teacher');
@@ -15,23 +18,40 @@ var Setting = mongoose.model('Setting');
 /**
  * Prepare for getting new schedule:
  *  - Update current schedule version number
+ *  - Return new number to sync app
  */
 exports.newVersion = function () {
-  // Update version number
-  // TO DO
-
   var socket = this;
 
   Setting
     .findOneAndUpdate({}, { $inc : { version : 1 } })
     .exec(function (err, setting) {
       if(err) console.log(err);
-
-      console.log(setting);
+      if(!setting) return;
       socket.emit('start_update', { version : setting.version });
     });
 };
 
+/**
+ * Process all items in the queue and
+ * add them to db
+ */
 exports.addEntry = function (entry) {
-  console.log(entry);
+  var socket = this;
+
+  queue.pushAndRun({
+    item : entry,
+    handler : function (item, next) {
+      var entry = {
+        date : {
+          start : item.date.start,
+          end : item.date.end
+        }
+      };
+
+      // TO DO: process item
+
+      next();
+    }
+  });
 };
